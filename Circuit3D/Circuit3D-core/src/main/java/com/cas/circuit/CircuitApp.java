26f3720e78d1f5fbaa585bf0bd2a6815b0d1f8af
张振宇 <sco_pra@126.com>
2017-07-05 20:00:06 +0800

    add

---
+++ Circuit3D/Circuit3D-core/src/main/java/com/cas/circuit/CircuitApp.java
@@ -0,0 +1,16 @@
+package com.cas.circuit;
+
+import com.jme3.app.SimpleApplication;
+
+public class CircuitApp extends SimpleApplication {
+
+	@Override
+	public void simpleInitApp() {
+		
+	}
+
+	public static void main(String[] args) {
+		new CircuitApp().start();
+	}
+
+}
